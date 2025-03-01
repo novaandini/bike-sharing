adccac9a0b2d0ac1610be2d371ec0ca676597097
novaandini <andininovaaa@gmail.com>
2025-03-01 15:33:29 +0800

    revised dashboard and notebook

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -68,6 +68,49 @@ def sum_orders_by_weathersit():
 
     st.pyplot(fig)
 
+def getDataByWeather(weatherSelection):
+    st.subheader(f"Tren pengguna layanan Bike Sharing pada cuaca {weatherSelection}")
+    weather_dict = {
+        "Cerah": 1,
+        "Berkabut": 2,
+        "Hujan": 3,
+        "Badai": 4
+    }
+
+    weatherSelection_num = weather_dict.get(weatherSelection, 0)
+
+    hour_df = pd.read_csv('dashboard/hour_data.csv')
+    hour_df['dteday'] = pd.to_datetime(hour_df['dteday'])
+    
+    df_filtered = hour_df[hour_df["weathersit"] == weatherSelection_num]
+
+    if not df_filtered.empty:
+    # Membuat line chart dengan Seaborn
+        fig, ax = plt.subplots(figsize=(8, 5))
+
+        hour_df["month_year"] = hour_df["dteday"].dt.to_period("M")
+
+        df_grouped = hour_df[hour_df["weathersit"]==weatherSelection_num].groupby(["month_year", "weathersit"])["cnt"].sum().reset_index()
+
+        # Konversi period ke string agar bisa dipakai sebagai label di grafik
+        df_grouped["month_year"] = df_grouped["month_year"].astype(str)
+
+        # Plot line chart dengan Seaborn
+        fig, ax = plt.subplots(figsize=(10, 5))
+        sns.lineplot(data=df_grouped, x="month_year", y="cnt", marker="o", ax=ax, color="blue")
+
+        
+        ax.set_title(f"Jumlah Customer Per Bulan Pada Cuaca {weatherSelection}")
+        ax.set_xlabel("Tanggal")
+        ax.set_ylabel("Jumlah Customer")
+        ax.tick_params(axis='x', rotation=45)
+        ax.grid(True)
+        
+        # Menampilkan grafik di Streamlit
+        st.pyplot(fig)
+    else:
+        st.warning("Tidak ada data untuk cuaca yang dipilih.")
+
 
 with st.sidebar:
     selectbox = st.selectbox(
@@ -75,17 +118,36 @@ with st.sidebar:
         options=('Tren pengguna dalam kuartal', 'Pengaruh cuaca terhadap pelanggan')
     )
 
+    if (selectbox == "Pengaruh cuaca terhadap pelanggan"):
+        weatherSelection = st.selectbox(
+            label="Pilih Cuaca",
+            options=('Seluruh Data', 'Cerah', 'Berkabut', 'Hujan', 'Badai')
+        )
+
+
 if (selectbox == "Tren pengguna dalam kuartal"):
     st.header('Tren pengguna layanan Bike Sharing :sparkles:')
 
     with st.container():
         sum_orders_df()
 elif (selectbox == "Pengaruh cuaca terhadap pelanggan"):
-    st.header('Pengaruh cuaca terhadap layanan Bike Sharing :sparkles:')
+    st.header('Pengaruh cuaca terhadap layanan Bike Sharing :sparkles:')\
 
     with st.container():
-        sum_orders_by_weathersit()
+        if (weatherSelection == 'Cerah'):
+            getDataByWeather('Cerah')
+        elif (weatherSelection == 'Berkabut'):
+            getDataByWeather('Berkabut')
+        elif (weatherSelection == 'Badai'):
+            getDataByWeather('Badai')
+        elif (weatherSelection == 'Hujan'):
+            getDataByWeather('Hujan')
+        elif (weatherSelection == 'Seluruh Data'):
+            sum_orders_by_weathersit()
+        else:
+            st.error('This is an error', icon="🚨")
 else:
     st.error('This is an error', icon="🚨")
+
 
 
